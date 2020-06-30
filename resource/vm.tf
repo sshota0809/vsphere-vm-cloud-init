87f2604e15349950a30bca271d34f9256b9333cd
sshota0809 <8736380+sshota0809@users.noreply.github.com>
2020-06-30 09:31:24 +0900

    modify gw address

--- resource/vm.tf
+++ resource/vm.tf
@@ -5,7 +5,7 @@ module "test-vm" {
     hostname          = "test-vm"
     resource_pool_id  = "${data.vsphere_resource_pool.infra.id}"
     network_address   = "10.0.0.1/24"
-    network_gateway   = "10.0.1.254"
+    network_gateway   = "10.0.0.254"
     datastore_id      = "${data.vsphere_datastore.storage.id}"
     network_id        = "${data.vsphere_network.server_nw.id}"
     cpu_num           = 2
